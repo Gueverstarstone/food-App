ffb7fa66de68555cf6a2ec77389c78c840ea4e21
gueverstarstone <josephmungai028@gmail.com>
2025-11-20 14:43:50 +0300

    created useEffect syntax

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import style from "./searchComponents.module.css";
 import { useState } from "react";
+import { useEffect } from "react";
 
 export default function Search() {
   const [query, setQuery] = useState("pizza");
+
+  // Syntax
+  // useEffect(()=>{},[])
+
+  useEffect(()=>{
+    function demo() {
+      console.log('use of useEffect')
+    }
+    demo();
+  },[query])
   return (
     <div>
       <input className={style.search} type="text" value={query} onChange={(e) => setQuery(e.target.value)}/>
