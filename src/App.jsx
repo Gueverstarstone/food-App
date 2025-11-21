3fe5434910773ad6e9041f9957184dca5b49c41d
gueverstarstone <josephmungai028@gmail.com>
2025-11-21 21:21:47 +0300

    created a recipe detail view

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import { useState } from "react";
 import Search from "./components/Search";
 import FoodList from "./components/FoodList";
 import Nav from "./components/Nav";
-import './App.css'
+import "./App.css";
+import Container from "./components/Container";
 
 function App() {
   const [foodData, setFoodData] = useState([]);
 
   return (
     <>
-    <Nav />
+      <Nav />
       <Search foodData={foodData} setFoodData={setFoodData} />
+      <Container>
+        <FoodList foodData={foodData} />
+      </Container>
       {/* //pass foodData as props */}
-      <FoodList foodData={foodData}/>
     </>
   );
 }
